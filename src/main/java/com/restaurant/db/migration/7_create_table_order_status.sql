e24e87bb9c72ba038b278261731fade38339fc40
Naitive13 <hei.titan.2@gmail.com>
2025-03-22 10:42:31 +0300

    fix: add missing ids

--- src/main/java/com/restaurant/db/migration/7_create_table_order_status.sql
+++ src/main/java/com/restaurant/db/migration/7_create_table_order_status.sql
@@ -1,7 +1,7 @@
-create type status as ENUM ('CREATED','CONFIRMED','IN_PROGRESS','DONE','DELIVERED');
+create type statusType as ENUM ('CREATED','CONFIRMED','IN_PROGRESS','DONE','DELIVERED');
 create table if not exists order_status (
     id bigint primary key,
     order_reference varchar(20) references "sortOrder"(order_reference),
-    order_status status,
+    order_status statusType,
     creation_date timestamp
 );
--- src/main/java/com/restaurant/db/migration/9_create_table_dish_order_status.sql
+++ src/main/java/com/restaurant/db/migration/9_create_table_dish_order_status.sql
@@ -1,6 +1,6 @@
 create table if not exists dish_order_status(
     id bigint primary key,
     dish_order_id bigint references dish_order(dish_order_id),
-    dish_order_status status,
+    dish_order_status statusType,
     creation_date timestamp
 );
