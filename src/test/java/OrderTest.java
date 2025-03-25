501a2e6ad63dc14a36110a123b02ac81a10669ff
Naitive13 <hei.titan.2@gmail.com>
2025-03-25 16:10:23 +0300

    test: add test for orders

---
+++ src/test/java/OrderTest.java
@@ -0,0 +1,58 @@
+import com.restaurant.dao.DishDAO;
+import com.restaurant.dao.OrderDAO;
+import com.restaurant.entities.*;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OrderTest {
+  OrderDAO subject = new OrderDAO();
+  DishDAO dishDAO = new DishDAO();
+
+  private Order newOrder(){
+
+    DishOrder dishOrder =
+            new DishOrder(1, dishDAO.get(new ArrayList<>(), 1, 10).getFirst(), "ORD002", 2);
+    Status status = new Status(StatusType.CREATED, LocalDateTime.of(2025, 1, 1, 0, 0, 0));
+    List<Status> statusList = new ArrayList<>();
+    statusList.add(status);
+    List<DishOrder> dishOrderList = new ArrayList<>();
+    dishOrderList.add(dishOrder);
+            return new Order("ORD002", LocalDateTime.of(2025, 1, 1, 0, 0, 0), dishOrderList, statusList);
+  }
+
+  @Test
+  void add_new_order() {
+    Order expected = newOrder();
+
+    subject.save(expected);
+    Order actual = subject.get(new ArrayList<>(), 1, 10).getFirst();
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void update_status() {
+    Order order = newOrder();
+    order.updateStatus();
+    Status expected = order.getActualStatus();
+
+    Status actual = new Status(StatusType.CONFIRMED, LocalDateTime.of(2025, 2, 1, 0, 0, 0));
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void get_total_amount() {
+    Order order = newOrder();
+    long expected = 30_000;
+
+    long actual = order.getTotalAmount();
+
+    assertEquals(expected, actual);
+  }
+}
